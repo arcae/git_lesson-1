b0d907bbdeb7b85a187ec8f19fddc5218cf61610
Arcae <o_regi@hotmail.com>
2017-01-21 07:43:27 -0500

    added SI function

--- src/myscript.py
+++ src/myscript.py
@@ -8,9 +8,13 @@ def percentage(z):
     ''' This fun calculates %'''
     return (z/100)
 
+def simpleInterest(p,t,r):
+    return (p*t*r)
+
 print(my_square(3))
+
 print (my_cube(4))
 print(percentage(5))
-
+print(simpleInterest(1000,2,0.05))
 
 
